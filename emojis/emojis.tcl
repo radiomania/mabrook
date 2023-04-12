360aa7688a1d26a3623a73b160439c15abd3e44b
MABROOK <83871957+radiomania@users.noreply.github.com>
2023-04-12 23:42:49 +0400

    Update emojis.tcl

--- emojis/emojis.tcl
+++ emojis/emojis.tcl
@@ -28,6 +28,7 @@
 # |                                                                                 |
 # +---------------------------------------------------------------------------------|
 
+
 # Choose your trigger for the bot to use ... use example set cmds "!" or "."# 
 set emojis "!"
 
@@ -46,9 +47,50 @@ bind pub - ${emojis}kiss1 pub_kiss1
 bind pub - ${emojis}kiss2 pub_kiss2
 bind pub - ${emojis}life pub_life
 
+##############################################################################
+#    DO NOT EDIT BELOW #
+##############################################################################
+package require Tcl 8.5
+encoding system utf-8
+# Ok, here is a problem:
+# We need all eggdrop commands.
+# The good thing is that all the eggdrop commands are in the global namespace.
+# The difficulty is to disingush between eggdrop commands
+# And Tcl commands.
+# To find out if it is a tcl command I just create an other interp, look at the commands there
+# and skip them
+# To make sure that this works, source this script as first script.
+# Otherwise there might be extra commands in the global namespace that we don't know.
+proc initUtf8 {} {
+   rename initUtf8 {}
+   set i [interp create]
+   set tcmds [interp eval $i {info commands}]
+   interp delete $i
+   set procs [info procs]
+   foreach cmd [info commands] {
+      if {   $cmd ni $tcmds && $cmd ni $procs
+         && "${cmd}_orig" ni [info commands]
+         && ![string match *_orig $cmd]
+      } {
+         # Eggdrop command.
+         rename $cmd ${cmd}_orig
+         interp alias {} $cmd {} fixutf8 ${cmd}_orig
+      }
+   }
+}
+initUtf8
+proc fixutf8 args {
+   set cmd {}
+   foreach arg $args {
+      lappend cmd [encoding convertto utf-8 $arg]
+   }
+   catch {{*}$cmd} res opt
+   dict incr opt -level
+   return -opt $opt $res
+}
 
 ##############################################################################
-#    EDIT BELOW #
+#    YOU CAN EDIT BELOW #
 ##############################################################################
  
 proc pub_list {nick uhost hand channel rest} {
